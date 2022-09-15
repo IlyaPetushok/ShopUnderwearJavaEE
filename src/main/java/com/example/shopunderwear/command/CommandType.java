090c944610a11ad274d18f30b6522ad81d7265b8
Petushok Ilya <a3310010752@mail.ru>
2022-09-15 23:53:34 +0300

    add AdminAddItem

--- src/main/java/com/example/shopunderwear/command/CommandType.java
+++ src/main/java/com/example/shopunderwear/command/CommandType.java
@@ -1,9 +1,11 @@
 package com.example.shopunderwear.command;
 
+import com.example.shopunderwear.command.impl.AdminAddItemModel;
 import com.example.shopunderwear.command.impl.AdminInputCommand;
 
 public enum CommandType {
-    ADMIN_INPUT_COMMAND(new AdminInputCommand());
+    ADMIN_INPUT_COMMAND(new AdminInputCommand()),
+    ADMIN_ADD_ITEM(new AdminAddItemModel());
 
     Command command;
 
